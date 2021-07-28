799262656d9f9233ca975f486dd7c550fb78258a
Yauheni Barsuk <barsuck.evgeni@gmail.com>
2021-07-29 01:50:18 +0300

    Added swagger to project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Invoice api swagger')
+    .setDescription('Documentation REST API')
+    .setVersion('1.0.0')
+    .addTag('InvoiceApiSwagger')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('/api/docs', app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
